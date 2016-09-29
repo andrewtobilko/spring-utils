ca1d029c0da8648ad99b01bc4c2d957b881a52ee
Andrew Tobilko <andrew.tobilko@gmail.com>
2016-09-29 23:58:42 +0300

    reflaction invoke() corrected

--- src/main/java/com/tobilko/spring/utils/dynamic/method/invocation/MethodCaller.java
+++ src/main/java/com/tobilko/spring/utils/dynamic/method/invocation/MethodCaller.java
@@ -1,5 +1,7 @@
 package com.tobilko.spring.utils.dynamic.method.invocation;
 
+import org.springframework.util.ReflectionUtils;
+
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
@@ -8,25 +10,42 @@ import java.util.Map;
 import java.util.stream.Stream;
 
 /**
+ * Standard method invocation by Reflection API by its name.
  *
  * Created by Andrew Tobilko on 9/29/2016.
  *
  */
 public class MethodCaller {
 
+
+    public static Object call(Object object, String methodName) {
+        return call(object, methodName, null, false);
+    }
+
     public static Object call(Object object, String methodName, Object... arguments) {
         return call(object, methodName, arguments, false);
     }
 
+    /**
+     *
+     * @param object - an instance to invoke a {@param method} on
+     * @param methodName - the method name
+     * @param arguments - method's arguments
+     * @param digDeep - whether only declared method are counted
+     *
+     * @return - an invocation method result
+     */
     public static Object call(Object object, String methodName, Object[] arguments, boolean digDeep) {
-        Class<?> c = object.getClass();
+        Object invocationResult = null;
         try {
+            Class<?> c = object.getClass();
             Method method = digDeep ? c.getDeclaredMethod(methodName) : c.getMethod(methodName);
-            method.invoke(object, arguments);
+            method.setAccessible(true);
+            invocationResult = method.invoke(object, arguments);
         } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
             e.printStackTrace();
         }
-        return null;
+        return invocationResult;
     }
 
 }
